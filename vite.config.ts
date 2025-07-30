56eabd0bf3bcd09ff61f25fcb4859f764894f4e4
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-07-30 14:14:53 +0530

    lets ee now

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const backendUrl = process.env.VITE_PRODUCTION == 'true' ? 'https://openfile-api.onrender.com' : "http://localhost:8000"
+const backendUrl = process.env.VITE_PRODUCTION == 'true'
+  ? process.env.VITE_BACKEND_APP_URL
+  : "http://localhost:8000"
+
+  console.log(backendUrl)
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
