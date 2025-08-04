3b78b9e18ec69f173d2a1e0a18a82969ab9a8e98
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-08-04 13:17:55 +0530

    ohh forgot to remove multithread upload now good

--- app/routes/upload/index.tsx
+++ app/routes/upload/index.tsx
@@ -119,28 +119,13 @@ function UploadPage() {
 
     try {
 
-      files.map(async (file) => {
-        const worker = new Worker(
-          new URL("../../utils/encryptWorker.ts", import.meta.url),
-          { type: "module" }
-        );
-
-        const encryptFileWithWorker = (file: File, secretKey: string, iv: string): Promise<Blob> => {
-          return new Promise((resolve, reject) => {
-            worker.postMessage({ file, secretKey, iv });
-
-            worker.onerror = (err) => reject("Worker error: " + err.message);
-            worker.onmessage = (event) => {
-              event.data.error ? reject(event.data.error) : resolve(event.data);
-            };
-          });
-        };
+      for (const file of files) {
         const status = fileStatusList.find((f) => f.name === file.name);
-        if (status?.status === "done") return;
+        if (status?.status === "done") continue;
 
         try {
           const mimeType = file.type || 'application/octet-stream';
-          const { url, key: s3Key } = await getUploadUrl(mimeType, token,file.size);
+          const { url, key: s3Key } = await getUploadUrl(mimeType, token, file.size);
           const encryptedBlob = await encryptFileWithWorker(file, key, iv);
           const encryptedFile = new File([encryptedBlob], file.name, { type: file.type });
 
@@ -152,7 +137,8 @@ function UploadPage() {
           const message = error instanceof Error ? error.message : String(error);
           setError(file.name, message ?? "error while upload this file");
         }
-      })
+
+      }
     } catch (error) {
       console.error("Upload process failed:", error);
       const message = error instanceof Error ? error.message : String(error);
